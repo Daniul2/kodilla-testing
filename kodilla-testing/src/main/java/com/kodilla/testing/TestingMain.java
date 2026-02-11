89594860abcff0088d37f83e2526a0b38f0a3e87
Daniil Medvediev <medvedievdaniil300@gmail.com>
2026-02-11 16:45:33 +0100

    6.2 Added Calculator and manual tests in TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(10, 5);
+        if (addResult == 15) {
+            System.out.println("Calculator test add: OK");
+        } else {
+            System.out.println("Calculator test add: Error!");
+        }
+
+        int subtractResult = calculator.subtract(10, 5);
+        if (subtractResult == 5) {
+            System.out.println("Calculator test subtract: OK");
+        } else {
+            System.out.println("Calculator test subtract: Error!");
+        }
     }
 }
