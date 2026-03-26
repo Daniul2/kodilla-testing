6f59a6d29cff5cefcf81f187acf7d43efba6d0c3
Daniil Medvediev <medvedievdaniil300@gmail.com>
2026-03-26 09:24:19 +0100

    Task 13.3: Board with Java Config and Fibonacci implementation with fixed tests

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/additional/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring.portfolio.additional;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Fibonacci {
+    public long calculateFibonacci(int n){
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+
+        long prev = 0;
+        long current = 1;
+
+        for (int i = 2; i <= n; i++) {
+            long next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return current;
+
+    }
+}
